f097568d09346d77748ed390743fb3045fd9dd73
Jose72 <jjjtony_72@hotmail.com>
2019-05-15 15:47:44 -0300

    added prefetch value to pipes + ack. now can run multiple containers with the same filter

--- src/server/pipe.py
+++ src/server/pipe.py
@@ -1,5 +1,6 @@
 import pika
 import json
+from time import sleep
 
 
 class Pipe:
@@ -11,6 +12,7 @@ class Pipe:
         self.q_name = q_name
         self.channel.queue_declare(queue=q_name, durable=True)
         self.routing_key = r_key
+        self.channel.basic_qos(prefetch_count=1)
 
     def send(self, message):
         self.channel.basic_publish(exchange='',
@@ -25,11 +27,16 @@ class Pipe:
             b = json.loads(body)
             result = processor.process(b)
             print(json.dumps(result))
+            sleep(0.1)
             if out_pipe:
                 out_pipe.send(json.dumps(result))
 
+            ch.basic_ack(delivery_tag=method.delivery_tag)
+
         self.channel.basic_consume(queue=self.q_name,
-                                   on_message_callback=callback,
-                                   auto_ack=True)
+                                   on_message_callback=callback)
 
         self.channel.start_consuming()
+
+    def close(self):
+        self.connection.close()
